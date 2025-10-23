5b82b24481e83ff0cdde8359685771a8b6df83d8
jsadler1050 <jadonsadler1@gmail.com>
2025-10-23 14:26:56 -0400

    MongoDB installed

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:BvV2oZs3f2BxClpGTbabzRwz@172.21.124.154:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
