a9fe92908e415eacffcb64b29fa2561e13bf7446
jsadler1050 <jadonsadler1@gmail.com>
2025-10-24 14:50:22 -0400

    Docker file and final files added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:BvV2oZs3f2BxClpGTbabzRwz@172.21.124.154:27017";
+const mongoURI =  "mongodb://root:XncwN9XDsJo1CACTMtULMBDw@172.21.96.229:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
