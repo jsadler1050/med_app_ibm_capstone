ce71b4a680f981899c022fe342cccd13f8d1bccf
jsadler1050 <jadonsadler1@gmail.com>
2025-10-24 08:30:19 -0400

    BookingConsultation directory added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import BookingConsultation from './Components/BookingConsultation/BookingConsultation';
 
 // Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
---
+++ src/Components/BookingConsultation.js
@@ -0,0 +1,1 @@
+./BookingConsultation/DoctorCard/DoctorCard
